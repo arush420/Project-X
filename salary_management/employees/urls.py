7f5cb6ede28fbd9a37079428dae199c17e30711d
vibhuti1403 <vibhutirajpal14@gmail.com>
2024-09-20 15:46:53 +1000

    Fixed emplyee home page issue

--- salary_management/employees/urls.py
+++ salary_management/employees/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import EmployeeListView, GenerateSalaryView, add_employee
+from .views import EmployeeListView, GenerateSalaryView, add_employee,home
 
 urlpatterns = [
-    path('', EmployeeListView.as_view(), name='employee_list'),  # Correct name
+    path('', EmployeeListView.as_view(), name='employee_list'),  # Correct name for the employee list
     path('generate_salary/', GenerateSalaryView.as_view(), name='generate_salary'),
     path('add_employee/', add_employee, name='add_employee'),
 ]
